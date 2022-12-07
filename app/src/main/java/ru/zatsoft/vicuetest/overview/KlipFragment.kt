5b8f9e51cd031c8eeabcce4d35b3034a01b5a892
Alexander <alex.zacepin@gmail.com>
2022-12-07 19:32:48 +0100

    added overlay text

--- app/src/main/java/ru/zatsoft/vicuetest/overview/KlipFragment.kt
+++ app/src/main/java/ru/zatsoft/vicuetest/overview/KlipFragment.kt
@@ -1,14 +1,16 @@
 package ru.zatsoft.vicuetest.overview
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import ru.zatsoft.vicuetest.bindImage
+import ru.zatsoft.vicuetest.R
 import ru.zatsoft.vicuetest.databinding.FragmentOverviewBinding
 
 class KlipFragment : Fragment() {
@@ -16,31 +18,45 @@ class KlipFragment : Fragment() {
     private val viewModel: KlipViewModel by viewModels()
     private var fragmentBinding : FragmentOverviewBinding? = null
 
-    /**
-     * Inflates the layout with Data Binding, sets its lifecycle owner to the KlipFragment
-     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
-     */
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentOverviewBinding.inflate(inflater)
         fragmentBinding = binding
-//        val binding = GridViewItemBinding.inflate(inflater)
-        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
-
-        // Giving the binding access to the KlipViewModel
         binding.viewModel = viewModel
+        binding.edText.visibility = View.GONE
         viewModel.photos.observe(viewLifecycleOwner){
-        bindImage(binding.imageView, viewModel.photos.value?.get(0)?.poster_url)}
+            configureVideoView(viewModel.photos.value?.get(0)?.file_url)
+        }
 
-        binding.photosGrid.adapter = PhotoGridAdapter { position  -> onListItemClick(position)}
+        val recyclerView: RecyclerView =   binding.photosGrid
+        recyclerView.adapter = PhotoGridAdapter { position  -> onListItemClick(position)}
+
+        binding.btnText.setOnClickListener{
+            binding.edText.visibility = View.VISIBLE
+        binding.edText.width = binding.frame1.width}
         return binding.root
     }
 
     private fun onListItemClick(position: Int) {
         fragmentBinding?.let {
-        bindImage(it.imageView, viewModel.photos.value?.get(position)?.poster_url)}
+            configureVideoView(viewModel.photos.value?.get(position)?.file_url)
+        }
     }
+
+    private fun configureVideoView(fileUrl: String?) {
+        fragmentBinding?.let{
+//            it.frame1.visibility = View.GONE
+                             it.videoView.setVideoPath(fileUrl)
+                             it.videoView.setOnPreparedListener {
+                                     video -> video.isLooping = true
+//                                     it.frame1.visibility = View.VISIBLE
+//                                     it.progressView?.visibility = View.INVISIBLE
+                                     video.start()}
+        }
+    }
+
 }
--- app/src/main/java/ru/zatsoft/vicuetest/overview/KlipViewModel.kt
+++ app/src/main/java/ru/zatsoft/vicuetest/overview/KlipViewModel.kt
@@ -1,38 +1,27 @@
 package ru.zatsoft.vicuetest.overview
 
-import android.widget.ImageView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ru.zatsoft.vicuetest.bindImage
 import ru.zatsoft.vicuetest.network.Api
 import ru.zatsoft.vicuetest.network.Klip
 
 class KlipViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<String>()
     private val _photos = MutableLiveData<List<Klip>>()
 
-    // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
     val photos: LiveData<List<Klip>> = _photos
     var initPhoto: String?
 
-    /**
-     * Call getPhotos() on init so we can display status immediately.
-     */
     init {
         getPhotos()
         initPhoto = photos.value?.get(0)?.poster_url
     }
 
-    /**
-     * Gets photos information from the API Retrofit service and updates the
-     * [Klip] [List] [LiveData].
-     */
     private fun getPhotos() {
         viewModelScope.launch {
             try{
